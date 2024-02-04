523815c02c51cebbbed756029b27a026c1a70e07
Adithyah Nair <adithyah.nair@gmail.com>
2024-02-04 18:15:20 +0530

    Ability to create new contract

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "acf9b656ac58d609"; }
+/******/ 		__webpack_require__.h = function() { return "34cf393843814639"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/CreateContractForm.js
+++ components/CreateContractForm.js
@@ -1,28 +1,85 @@
 import React, { Component } from "react";
-import { FormField, Form, Button, Input } from "semantic-ui-react";
+import { FormField, Form, Button, Input, Message } from "semantic-ui-react";
+import web3 from "../ethereum/web3";
+import factory from "../ethereum/factory";
+import Router from "../routes";
 
 class CreateContractForm extends Component {
+  state = {
+    managerAddress: "",
+    farmerAddress: "",
+    name: "",
+    quantity: "",
+    errorMessage: "",
+    loading: false,
+  };
+  onCreate = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ loading: true, errorMessage: "" });
+    try {
+      await factory.methods
+        .createContract(
+          this.state.farmerAddress,
+          this.state.name,
+          this.state.quantity
+        )
+        .send({
+          from: accounts[0],
+        });
+      Router.replaceRoute("/contractlist");
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false });
+  };
   render() {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.onCreate} error={!!this.state.errorMessage}>
           <FormField>
             <label>Your Address</label>
-            <Input />
+            <Input
+              onChange={(event) =>
+                this.setState({ managerAddress: event.target.value })
+              }
+              value={this.state.managerAddress}
+            />
           </FormField>
           <FormField>
             <label>Farmer's Address</label>
-            <Input />
+            <Input
+              onChange={(event) =>
+                this.setState({ farmerAddress: event.target.value })
+              }
+              value={this.state.farmerAddress}
+            />
           </FormField>
           <FormField>
             <label>Product Name</label>
-            <Input />
+            <Input
+              onChange={(event) => this.setState({ name: event.target.value })}
+              value={this.state.name}
+            />
           </FormField>
           <FormField>
             <label>Quantity</label>
-            <Input label="Metric Ton" labelPosition="right" />
+            <Input
+              onChange={(event) =>
+                this.setState({ quantity: event.target.value })
+              }
+              value={this.state.quantity}
+              label="Metric Ton"
+              labelPosition="right"
+            />
           </FormField>
-          <Button content="Create" primary />
+          <Button loading={this.state.loading} content="Create" primary />
+          <Message
+            header="Oops!"
+            content={this.state.errorMessage}
+            error
+            negative
+          />
         </Form>
       </div>
     );
