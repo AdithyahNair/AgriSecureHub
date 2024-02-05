d66f11e896d2102d31c05b45fd7c33205e3e0977
Adithyah Nair <adithyah.nair@gmail.com>
2024-02-05 09:54:56 +0530

    Added back button to createContract page

--- pages/contractlist/new.js
+++ pages/contractlist/new.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
 import CreateContractForm from "../../components/CreateContractForm";
+import { Link } from "../../routes";
 
 class CreateContract extends Component {
   render() {
     return (
       <Layout>
+        <Link route="/contractlist/true">
+          <a>Back</a>
+        </Link>
         <h3 style={{ marginBottom: "30px" }}>Create Contract</h3>
         <CreateContractForm />
       </Layout>
