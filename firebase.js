96f4f8123212c9493656bf6f3ab4ddb173059cf9
Adithyah Nair <adithyah.nair@gmail.com>
2024-02-22 13:58:30 +0530

    Installed Firebase and added config

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeT2sbKUepAzBVrmakBgl9qBO36b1xfQk",
+  authDomain: "agrihub-dafcf.firebaseapp.com",
+  databaseURL:
+    "https://agrihub-dafcf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "agrihub-dafcf",
+  storageBucket: "agrihub-dafcf.appspot.com",
+  messagingSenderId: "558287744819",
+  appId: "1:558287744819:web:7882579d4514188928d846",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
